c661bf2443ce1171ccf3d5ce7cfc5ec854f04d00
P0S1T1VE GUY <UriyDoroshenko@ya.ru>
2024-10-27 20:02:37 +0300

    urls corrected

--- anime/urls.py
+++ anime/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
 
-from .views import home, anime
+from .views import home, animelist, anime, anime_episode
 
 
 app_name = 'anime'
 urlpatterns = [
     path('', home, name='home'),
-    path('anime/', anime, name='anime'),
+    path('anime/', animelist, name='animelist'),
+    path('anime/<str:anime_name>/', anime, name='anime'),
+    path('anime/<str:anime_name>/<int:episode>/', anime_episode, name='anime_episode'),
 ]
--- mail/urls.py
+++ mail/urls.py
@@ -7,5 +7,5 @@ from .views import mailbox, show_message
 app_name = 'mail'
 urlpatterns = [
     path('mail/', mailbox, name='mailbox'),
-    path('mail/@<str:message_id>/', show_message, name='show_message'),
+    path('mail/<str:message_id>/', show_message, name='show_message'),
 ]
