7741aa40a207d2b60089865bcd0750b5637748a9
P0S1T1VE GUY <UriyDoroshenko@ya.ru>
2024-10-28 18:31:23 +0300

    theme functional added

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import get_mail_msgs, get_videos, get_chat_msgs
+from .views import get_mail_msgs, get_videos, get_chat_msgs, post_user_theme
 
 
 app_name = 'api'
 urlpatterns = [
-    path('get/mail-messages', get_mail_msgs, name='info'),
-    path('get/videos', get_videos, name='info'),
-    path('get/chat-messages', get_chat_msgs, name='info'),
-    #path('post/', post, name='post'),
+    path('get/mail-messages', get_mail_msgs, name='get_mail_msgs'),
+    path('get/videos', get_videos, name='get_videos'),
+    path('get/chat-messages', get_chat_msgs, name='get_chat_msgs'),
+    path('post/', post_user_theme, name='post_user_theme'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+
+from AUTH.models import User
+
 
 def get_mail_msgs(request):
 	return HttpResponse('mail')
@@ -7,3 +11,15 @@ def get_chat_msgs(request):
 	return HttpResponse('chat')
 def get_videos(request):
 	return HttpResponse('videos')
+
+def post_user_theme(request):
+	type = request.POST.get('type')
+	username = request.POST.get('username')
+	theme = request.POST.get('theme')
+
+	if type == 'set-default-theme':
+		if theme in ['dark', 'light', '0', '1']:
+			theme = theme in ['dark', '0']
+			user = get_object_or_404(User, username=username)
+			user.is_dark_theme = theme
+			user.save()
